b52015e0045cdfb9406daf3df5e21a671f7367af
Mostafa Hosseini <m.hosseini.m69@gmail.com>
2023-03-17 17:42:39 +0330

    update chatcontact

--- server.cjs
+++ server.cjs
@@ -8,22 +8,22 @@ const signin = require('./controllers/signin.cjs');
 const profile = require('./controllers/profile.cjs');
 const image = require('./controllers/image.cjs');
 
-const db = require('knex')({
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  searchPath: ['knex', 'public'],
-});
+// const db = require('knex')({
+//   client: 'pg',
+//   connection: process.env.PG_CONNECTION_STRING,
+//   searchPath: ['knex', 'public'],
+// });
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//       host : 'dpg-cg9gh99mbg54mbeu520g-a',
-//       port : 5432,
-//       user : 'mostafa',
-//       password : 'Zl9VqgO2xLrVdasAPpGOcX8FITq1xhAz',
-//       database : 'smartbrain_9ua8'
-//     }
-//   });
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : 'dpg-cg9gh99mbg54mbeu520g-a',
+      port : 5432,
+      user : 'mostafa',
+      password : 'Zl9VqgO2xLrVdasAPpGOcX8FITq1xhAz',
+      database : 'smartbrain_9ua8'
+    }
+  });
 
 const app = express();
 

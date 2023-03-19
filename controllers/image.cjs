4b43c45a66ff415e933af3d64c73685d5779c1ce
Mostafa Hosseini <m.hosseini.m69@gmail.com>
2023-03-19 19:50:09 +0330

    update chatcontact

--- controllers/image.cjs
+++ controllers/image.cjs
@@ -1,20 +1,4 @@
-const Clarifai = require('clarifai');
-
-const app = new Clarifai.App({
-    apiKey: '714d3f9392cc4f45bb10a44d51175df0'
-});
-
-const handleApiCall = (req, res) => {
-    app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(400).json('unable to work with API')})
-}
-   
+ 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -29,5 +13,4 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
     handleImage,
-    handleApiCall
 };
